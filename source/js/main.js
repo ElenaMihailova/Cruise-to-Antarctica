9d6b3e3733f0d1b2084899f9f068e8b8ef4ae56c
Elena <pumpkinpum@gmail.com>
2022-11-15 22:56:15 +0300

    scroll

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,7 @@
+import {iosVhFix} from './utils/ios-vh-fix';
+import {initModals} from './modules/modals/init-modals';
+import {scrollLock} from './utils/scroll-lock';
+
 document.querySelector('.nojs').classList.remove('nojs');
 
 const openMenu = (buttonElem, menuElem) => {
@@ -7,6 +11,7 @@ const openMenu = (buttonElem, menuElem) => {
   button.addEventListener('click', (elem) => {
     elem.preventDefault();
     menu.classList.toggle('header--opened');
+    scrollLock.disableScrolling();
   });
 };
 
@@ -42,3 +47,48 @@ window.onload = function () {
     }
   });
 };
+
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initModals();
+  });
+});
+
+// ---------------------------------
+
+// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+
+// привязывайте js не на классы, а на дата атрибуты (data-validate)
+
+// вместо модификаторов .block--active используем утилитарные классы
+// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
+// .select.select--opened ❌ ---> [data-select].is-open ✅
+
+// выносим все в дата атрибуты
+// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+
+// для адаптивного JS используется matchMedia и addListener
+// const breakpoint = window.matchMedia(`(min-width:1024px)`);
+// const breakpointChecker = () => {
+//   if (breakpoint.matches) {
+//   } else {
+//   }
+// };
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+// используйте .closest(el)
--- source/js/modules/modals/modals.js
+++ source/js/modules/modals/modals.js
@@ -205,3 +205,5 @@ export class Modals {
     this._enableScrolling = true;
   }
 }
+
+console.log('текст из modal');
